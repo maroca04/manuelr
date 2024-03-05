eb040831068a0322243d9b2c2f5b7c4a7e8b8c8f
Manu <Manuelrosacanelo@gmail.com>
2024-03-05 09:59:00 -0400

    Finalizado 1.sh

--- 1.sh
+++ 1.sh
@@ -2,5 +2,18 @@
 echo "dime una nota"
 read nota 
 
-if [$nota -ge 9 ]; then  
-    echo "Sobresaliente";
+if  [ "$nota" -ge 9 ]; then
+echo "Sobresaliente"
+fi;
+
+if  [ "$nota" -ge 5 ]; then
+echo "Aprovado"
+fi;
+
+if  [ "$nota" -lt 5 ]; then
+echo "Suspendido"
+fi;
+
+
+
+
